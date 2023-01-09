19d7ba2b69786e8a53831bfd87a215c5463e7da9
Mario Rizki <mariorizki07@gmail.com>
2023-01-09 18:36:14 +0700

    make base style for discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,12 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => (
+  <div className="flex flex-col">
+    <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10"></div>
+    <h2 className="font-bold text-3xl text-white text-center">Discover</h2>
+    <select onChange={() => {}} value="" className="bg-black"></select>
+  </div>
+);
 
 export default Discover;
